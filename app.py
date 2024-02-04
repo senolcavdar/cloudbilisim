86f847f27a10a4793ec922a71ee8ee45beb937c3
Şenol Çavdar <senol-hasretim@windowslive.com>
2024-02-04 19:04:08 +0300

    Project changed

--- app.py
+++ app.py
@@ -1,10 +1,47 @@
 from flask import Flask
+from flask_restful import Api, Resource
+import pandas as pd
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route("/")
-def hello_world():
-    return "Hello world"
+class Quotes(Resource):
+    def get(self):
+        data = pd.read_csv('quotes.csv')
+        data = data.to_dict('records')
+        return {'data' : data}, 200
+
+    def post(self):
+        date = request.args['date']
+        quotes = request.args['quotes']
+        theme = request.args['theme']
+
+        data = pd.read_csv('quotes.csv')
+
+        new_data = pd.DataFrame({
+            'date': [date],
+            'quotes': [quotes],
+            'theme': [theme]
+        })
+        data = data.append(new_data, ignore_index=True)
+        data.to_csv('quotes.csv', index=False)
+        return {'data': new_data.to_dict('records')}, 200
+
+class Date(Resource):
+    def get(self, date):
+        data = pd.read_csv('quotes.csv')
+        data = data.to_dict('records')
+        
+        matching_entries = []
+        for entry in data:
+            # Karşılaştırma yaparken tür uyumsuzluğu önlemek için str() kullan
+            if entry['date'] == date:
+                matching_entries.append(entry)
+        
+        if matching_entries:
+            return {'data': matching_entries}, 200
+        else:
+            return {'message': 'Girilen tarihte bir soz bulunamadi !'}, 404
 
 if __name__ == "__main__":
     app.run( host='0.0.0.0')
